53fb6e39b29d541a92470ba351da1ed56b7f0495
Kalmár Dániel <daniel.kalmar@moresimp.com>
2020-01-03 17:15:32 +0100

    add customers update

--- customers/urls.py
+++ customers/urls.py
@@ -3,6 +3,7 @@ from customers import views
 
 urlpatterns = [
     path('', views.index, name='customers_list'),
-    path('create/', views.create_customer, name='create_customer')
+    path('create/', views.create_customer, name='create_customer'),
+    path('update/<int:id>', views.update_customer, name='update_customer'),
 
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
 from .models import Customer
@@ -22,3 +22,15 @@ def create_customer(request):
         form = CustomerForm()
 
     return render(request, 'customers_create.html', {'form': form})
+
+
+def update_customer(request, id=None, template_name='form.html'):
+    id = get_object_or_404(Customer, pk=id)
+    form = CustomerForm(request.POST or None, instance=id)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('customers_list'))
+
+    context = {'form': form}
+
+    return render(request, 'customers_update.html', context)
